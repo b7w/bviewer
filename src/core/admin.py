8b889d8c32ea8ac6ae829e2f7dc54a1215946131
B7W <b7w@isudo.ru>
2012-03-07 21:21:15 +0400

    Add url to profile, like name but lower case. Add new top gallery appending after proxy user create. Make admin more pretty and clever.

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,20 +1,102 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from models import Gallery, Image, Video
+from django.contrib.auth.admin import UserAdmin
+from django.forms import models
 
-class GalleryAdmin( admin.ModelAdmin ):
-    pass
+from models import ProxyUser, Gallery, Image, Video
+
+class ModelAdmin( admin.ModelAdmin ):
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Hack to save obj instance to self.user
+        """
+        self.object = obj
+        return super( ModelAdmin, self ).get_form( request, obj, **kwargs )
+
+
+class GalleryAdmin( ModelAdmin ):
+    list_select_related = True
+    list_display = ("title", "parent", "user", "time",)
+    list_filter = (  "parent__title", "user__username", "time", )
+    ordering = ("parent", "user", "time",)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent":
+            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.user.id )
+        return super( GalleryAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
 
 admin.site.register( Gallery, GalleryAdmin )
 
 
-class ImageAdmin( admin.ModelAdmin ):
-    pass
+class ImageAdmin( ModelAdmin ):
+    list_select_related = True
+    list_display = ("gallery_title", "gallery_user", "path", )
+    list_filter = ( "gallery__title", "gallery__user__username", )
+    ordering = ("gallery__user__username", "path",)
+
+    def gallery_title(self, obj):
+        return obj.gallery.title
+
+    def gallery_user(self, obj):
+        return obj.gallery.user.username
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "gallery":
+            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.gallery.user.id )
+        return super( ImageAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
 
 admin.site.register( Image, ImageAdmin )
 
-class VideoAdmin( admin.ModelAdmin ):
-    pass
+
+class VideoAdmin( ModelAdmin ):
+    list_select_related = True
+    list_display = ("gallery_title", "gallery_user", "title", "uid",)
+    list_filter = ( "gallery__title", "gallery__user__username", )
+    ordering = ("gallery__user__username", "uid",)
+
+    def gallery_title(self, obj):
+        return obj.gallery.title
+
+    def gallery_user(self, obj):
+        return obj.gallery.user.username
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        Show in drop down menu only user galleries and images
+        """
+        if db_field.name == "gallery":
+            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.gallery.user.id )
+        return super( VideoAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
 
 admin.site.register( Video, VideoAdmin )
+
+
+class ProxyUserForm( models.ModelForm ):
+    class Meta:
+        model = ProxyUser
+
+
+class UserAdmin( UserAdmin, ModelAdmin ):
+    list_select_related = True
+    list_display = ("username", "email", "is_staff", "home", "top_gallery", )
+    fieldsets = (
+        ("Main", {"fields": ("username", "email", "password",)}),
+        ("Personal info", {"fields": ("url", "home", "top_gallery", "about_title", "about_text", "avatar",)}),
+        ("Permissions", {"fields": ("is_active", "is_staff",)}),
+        ("Important dates", {"fields": ("last_login", "date_joined",)}),
+        )
+    readonly_fields = ( "password", "is_active", "is_staff", "last_login", "date_joined", )
+    form = ProxyUserForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        Show in drop down menu only user galleries and images
+        """
+        if db_field.name == "top_gallery":
+            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.id )
+        if db_field.name == "avatar":
+            kwargs["queryset"] = Image.objects.filter( gallery__user__id=self.object.id )
+        return super( UserAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
+
+admin.site.register( ProxyUser, UserAdmin )
