f3ea6bfab56dd66103a8487c78f8eb1fba49178b
B7W <b7w@isudo.ru>
2012-03-12 21:59:26 +0400

    Add CacheImageAsync that send resizing to celery. Change a little pre cache album image, it is now async so no need more than one option per list of images.

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -10,3 +10,15 @@ from core.management.commands.clearcache import ClearCache
 def clear_cache():
     cache = ClearCache( path=settings.VIEWER_CACHE_PATH )
     cache.clear( )
+
+
+@task
+def cache_image_process( image ):
+    image.process( )
+    return image
+
+
+@task
+def cache_image_download( image ):
+    image.download( )
+    return image
