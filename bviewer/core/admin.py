b01590a539fe2722a3f4204ca9e5aae68a20fbc2
B7W <isudoblog@gmail.com>
2012-11-03 21:43:03 +0400

    Add private field to gallery. Add check on all views to show private only for onwer.
    No any change in api or profile.

--- bviewer/core/admin.py
+++ bviewer/core/admin.py
@@ -9,31 +9,31 @@ from django.forms import models
 from bviewer.core.utils import RaisingRange
 from bviewer.core.models import ProxyUser, Gallery, Image, Video
 
-class ModelAdmin( admin.ModelAdmin ):
+class ModelAdmin(admin.ModelAdmin):
     def get_form(self, request, obj=None, **kwargs):
         """
         Hack to save obj instance to self.user
         """
         self.object = obj
-        return super( ModelAdmin, self ).get_form( request, obj, **kwargs )
+        return super(ModelAdmin, self).get_form(request, obj, **kwargs)
 
 
-class GalleryAdmin( ModelAdmin ):
+class GalleryAdmin(ModelAdmin):
     list_select_related = True
-    list_display = ("title", "parent", "user", "time",)
+    list_display = ("title", "parent", "user", "private", "time",)
     list_filter = (  "parent__title", "user__username", "time", )
-    ordering = ("parent", "user", "time",)
+    ordering = ("user", "parent", "time",)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         print self.object
         if db_field.name == "parent" and self.object:
-            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.user.id )
-        return super( GalleryAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
+            kwargs["queryset"] = Gallery.objects.filter(user__id=self.object.user.id)
+        return super(GalleryAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-admin.site.register( Gallery, GalleryAdmin )
+admin.site.register(Gallery, GalleryAdmin)
 
 
-class ImageAdmin( ModelAdmin ):
+class ImageAdmin(ModelAdmin):
     list_select_related = True
     list_display = ("gallery_title", "gallery_user", "path", )
     list_filter = ( "gallery__title", "gallery__user__username", )
@@ -47,13 +47,13 @@ class ImageAdmin( ModelAdmin ):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "gallery" and self.object:
-            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.gallery.user.id )
-        return super( ImageAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
+            kwargs["queryset"] = Gallery.objects.filter(user__id=self.object.gallery.user.id)
+        return super(ImageAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-admin.site.register( Image, ImageAdmin )
+admin.site.register(Image, ImageAdmin)
 
 
-class VideoAdmin( ModelAdmin ):
+class VideoAdmin(ModelAdmin):
     list_select_related = True
     list_display = ("gallery_title", "gallery_user", "title", "uid",)
     list_filter = ( "gallery__title", "gallery__user__username", )
@@ -70,29 +70,29 @@ class VideoAdmin( ModelAdmin ):
         Show in drop down menu only user galleries and images
         """
         if db_field.name == "gallery" and self.object:
-            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.gallery.user.id )
-        return super( VideoAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
+            kwargs["queryset"] = Gallery.objects.filter(user__id=self.object.gallery.user.id)
+        return super(VideoAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-admin.site.register( Video, VideoAdmin )
+admin.site.register(Video, VideoAdmin)
 
 
-class ProxyUserForm( models.ModelForm ):
+class ProxyUserForm(models.ModelForm):
     """
     Set for UserAdmin ProxyUser model except User.
     Add choice field for cache size filed.
     """
 
     def __init__(self, *args, **kwargs):
-        self.range = RaisingRange( 512, start=16, base=16 )
+        self.range = RaisingRange(512, start=16, base=16)
         self.choice = [(i, "%s MB" % i) for i in self.range]
-        super( ProxyUserForm, self ).__init__( *args, **kwargs )
-        self.fields['cache_size'] = forms.ChoiceField( choices=self.choice )
+        super(ProxyUserForm, self).__init__(*args, **kwargs)
+        self.fields['cache_size'] = forms.ChoiceField(choices=self.choice)
 
     class Meta:
         model = ProxyUser
 
 
-class UserAdmin( UserAdmin, ModelAdmin ):
+class UserAdmin(UserAdmin, ModelAdmin):
     list_select_related = True
     list_display = ("username", "email", "is_staff", "home", "top_gallery", )
     fieldsets = (
@@ -109,9 +109,9 @@ class UserAdmin( UserAdmin, ModelAdmin ):
         Show in drop down menu only user galleries and images
         """
         if db_field.name == "top_gallery" and self.object:
-            kwargs["queryset"] = Gallery.objects.filter( user__id=self.object.id )
+            kwargs["queryset"] = Gallery.objects.filter(user__id=self.object.id)
         if db_field.name == "avatar" and self.object:
-            kwargs["queryset"] = Image.objects.filter( gallery__user__id=self.object.id )
-        return super( UserAdmin, self ).formfield_for_foreignkey( db_field, request, **kwargs )
+            kwargs["queryset"] = Image.objects.filter(gallery__user__id=self.object.id)
+        return super(UserAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-admin.site.register( ProxyUser, UserAdmin )
+admin.site.register(ProxyUser, UserAdmin)
