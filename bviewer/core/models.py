ee671e4be9b93649b2e2e044646c7b897a2254bc
B7W <b7w@isudo.ru>
2012-11-03 21:43:03 +0400

    Add private field to gallery. Add check on all views to show private only for onwer.
    No any change in api or profile.

--- bviewer/core/models.py
+++ bviewer/core/models.py
@@ -11,28 +11,28 @@ from django.utils.html import escape
 from django.utils import simplejson
 
 
-class ProxyManager( models.Manager ):
+class ProxyManager(models.Manager):
     """
     Adds safe_get, that return obj or none
     """
 
     def safe_get(self, **kwargs):
         try:
-            return self.get( **kwargs )
+            return self.get(**kwargs)
         except self.model.DoesNotExist:
             return None
 
 
-class ProxyUser( User ):
-    url = models.CharField( max_length=16, unique=True )
-    home = models.CharField( max_length=256, null=True, blank=True )
-    cache_size = models.PositiveIntegerField( default=32, validators=[MinValueValidator( 16 ), MaxValueValidator( 256 )] )
-    top_gallery = models.ForeignKey( "Gallery", related_name="top", null=True, blank=True )
-    about_title = models.CharField( max_length=256, blank=True )
-    about_text = models.TextField( max_length=1024, blank=True )
-    avatar = models.ForeignKey( "Image", related_name="avatar", null=True, blank=True )
+class ProxyUser(User):
+    url = models.CharField(max_length=16, unique=True)
+    home = models.CharField(max_length=256, null=True, blank=True)
+    cache_size = models.PositiveIntegerField(default=32, validators=[MinValueValidator(16), MaxValueValidator(256)])
+    top_gallery = models.ForeignKey("Gallery", related_name="top", null=True, blank=True)
+    about_title = models.CharField(max_length=256, blank=True)
+    about_text = models.TextField(max_length=1024, blank=True)
+    avatar = models.ForeignKey("Image", related_name="avatar", null=True, blank=True)
 
-    objects = ProxyManager( )
+    objects = ProxyManager()
 
     def for_json(self):
         data = {
@@ -47,14 +47,19 @@ class ProxyUser( User ):
                 "avatar": self.avatar_id,
                 "about_text": self.about_text,
                 "about_title": self.about_title,
-                }
+            }
         }
         return data
 
     def save(self, force_insert=False, force_update=False, using=None):
         if not self.url:
-            self.url = self.username.lower( )
-        super( ProxyUser, self ).save( force_insert, force_update, using )
+            self.url = self.username.lower()
+        super(ProxyUser, self).save(force_insert, force_update, using)
+
+    def __eq__(self, other):
+        if other and other.is_authenticated():
+            return self.id == other.id
+        return False
 
 
     class Meta:
@@ -69,28 +74,32 @@ class ProxyUser( User ):
 
 def add_top_gallery(sender, instance, created, **kwargs):
     if created:
-        gal = Gallery( user=instance, title="Welcome", description="Edit main gallery to change it" )
-        gal.save( )
+        gal = Gallery(user=instance, title="Welcome", description="Edit main gallery to change it")
+        gal.save()
         instance.top_gallery = gal
-        instance.save( )
+        instance.save()
 
-post_save.connect( add_top_gallery, sender=ProxyUser )
+post_save.connect(add_top_gallery, sender=ProxyUser)
 
 
-class Gallery( models.Model ):
-    parent = models.ForeignKey( "self", null=True, blank=True, related_name="children" )
-    title = models.CharField( max_length=256 )
-    user = models.ForeignKey( ProxyUser )
-    description = models.TextField( max_length=512, null=True, blank=True )
-    thumbnail = models.ForeignKey( "Image", null=True, blank=True, related_name="thumbnail" )
-    time = models.DateTimeField( default=datetime.now )
+class Gallery(models.Model):
+    parent = models.ForeignKey("self", null=True, blank=True, related_name="children")
+    title = models.CharField(max_length=256)
+    user = models.ForeignKey(ProxyUser)
+    private = models.BooleanField(default=False)
+    description = models.TextField(max_length=512, null=True, blank=True)
+    thumbnail = models.ForeignKey("Image", null=True, blank=True, related_name="thumbnail")
+    time = models.DateTimeField(default=datetime.now)
 
-    objects = ProxyManager( )
+    objects = ProxyManager()
 
     @classmethod
-    def get_galleries(cls, top_id ):
-        query = Gallery.objects.filter( Q( id=top_id ) | Q( parent=top_id ) )
-        if not len( query ):
+    def get_galleries(cls, top_id, private=None):
+        if private is not None:
+            query = Gallery.objects.filter(Q(private=private), Q(id=top_id) | Q(parent=top_id))
+        else:
+            query = Gallery.objects.filter(Q(id=top_id) | Q(parent=top_id))
+        if not len(query):
             return None, []
         main = None
         galleries = []
@@ -98,23 +107,24 @@ class Gallery( models.Model ):
             if item.id == top_id:
                 main = item
             else:
-                galleries.append( item )
+                galleries.append(item)
         return main, galleries
 
     def for_json(self):
         data = {
             "id": self.id,
             "title": self.title,
+            "private": self.private,
             "description": self.description,
             "time": self.time,
-            }
+        }
         if self.thumbnail:
             data["thumbnail"] = self.thumbnail_id
         return data
 
     def __unicode__(self):
         if self.parent:
-            return self.title + u" -> " + unicode( self.parent )
+            return self.title + u" -> " + unicode(self.parent)
         return self.title
 
     class Meta:
@@ -124,35 +134,35 @@ class Gallery( models.Model ):
         unique_together = (("title", "user"),)
 
 
-class Image( models.Model ):
-    gallery = models.ForeignKey( Gallery )
-    path = models.CharField( max_length=256 )
+class Image(models.Model):
+    gallery = models.ForeignKey(Gallery)
+    path = models.CharField(max_length=256)
 
-    objects = ProxyManager( )
+    objects = ProxyManager()
 
     def for_json(self):
         data = {
             "id": self.id,
             "gallery": self.gallery_id,
             "path": self.path,
-            }
+        }
         return data
 
     def __unicode__(self):
-        return unicode( self.gallery.title ) + u": " + self.path
+        return unicode(self.gallery.title) + u": " + self.path
 
     class Meta:
         ordering = ["path"]
         unique_together = (("gallery", "path"),)
 
 
-class Video( models.Model ):
-    uid = models.CharField( max_length=32 )
-    gallery = models.ForeignKey( Gallery )
-    title = models.CharField( max_length=256 )
-    description = models.TextField( max_length=512, null=True, blank=True )
+class Video(models.Model):
+    uid = models.CharField(max_length=32)
+    gallery = models.ForeignKey(Gallery)
+    title = models.CharField(max_length=256)
+    description = models.TextField(max_length=512, null=True, blank=True)
 
-    objects = ProxyManager( )
+    objects = ProxyManager()
 
     def for_json(self):
         data = {
@@ -161,7 +171,7 @@ class Video( models.Model ):
             "gallery": self.gallery_id,
             "title": self.title,
             "description": self.description,
-            }
+        }
         return data
 
     @property
@@ -169,16 +179,16 @@ class Video( models.Model ):
         """
         Build escaped url to video
         """
-        return escape( "http://player.vimeo.com/video/" + self.uid )
+        return escape("http://player.vimeo.com/video/" + self.uid)
 
     @property
     def thumbnail_url(self):
         """
         Get video thumbnail url.
         """
-        url = "http://vimeo.com/api/v2/video/{0}.json".format( self.uid )
-        json = urllib2.urlopen( url ).read( )
-        info = simplejson.loads( json, encoding="UTF-8" ).pop( )
+        url = "http://vimeo.com/api/v2/video/{0}.json".format(self.uid)
+        json = urllib2.urlopen(url).read()
+        info = simplejson.loads(json, encoding="UTF-8").pop()
         return info["thumbnail_large"]
 
     def __unicode__(self):
--- bviewer/core/views.py
+++ bviewer/core/views.py
@@ -15,7 +15,7 @@ from bviewer.core.models import Gallery, Image, Video
 
 import logging
 
-logger = logging.getLogger( __name__ )
+logger = logging.getLogger(__name__)
 
 
 @cache_page(60 * 60)
@@ -24,20 +24,21 @@ def ShowHome( request, user=None ):
     """
     Show home pages with galleries
     """
-    holder, user_url = get_gallery_user( request, user )
+    holder, user_url = get_gallery_user(request, user)
     if not holder:
-        return ShowMessage( request, message="No user defined" )
+        return ShowMessage(request, message="No user defined")
 
-    main, galleries = Gallery.get_galleries( holder.top_gallery_id )
+    private = None if holder == request.user else False
+    main, galleries = Gallery.get_galleries(holder.top_gallery_id, private)
     if not main:
-        return ShowMessage( request, message="No main gallery" )
+        return ShowMessage(request, message="No main gallery")
 
-    return render( request, "core/galleries.html", {
+    return render(request, "core/galleries.html", {
         'path': request.path,
         'main': main,
         'galleries': galleries,
         'user_url': user_url,
-        } )
+    })
 
 
 @cache_page(60 * 60)
@@ -46,24 +47,25 @@ def ShowGallery( request, id, user=None ):
     """
     Show sub galleries or images with videos
     """
-    id = int( id )
-    holder, user_url = get_gallery_user( request, user )
+    id = int(id)
+    holder, user_url = get_gallery_user(request, user)
     if not holder:
-        return ShowMessage( request, message="No user defined" )
+        return ShowMessage(request, message="No user defined")
 
-    main, galleries = Gallery.get_galleries( id )
+    private = None if holder == request.user else False
+    main, galleries = Gallery.get_galleries(id, private)
     if not main:
-        return ShowMessage( request, message="No main gallery" )
+        return ShowMessage(request, message="No main gallery")
 
     videos = []
     images = []
     template = u"core/galleries.html"
-    if not len( galleries ):
+    if not len(galleries):
         template = u"core/gallery.html"
-        videos = Video.objects.filter( gallery__user__id=holder.id, gallery=id )
-        images = Image.objects.filter( gallery__user__id=holder.id, gallery=id )
+        videos = Video.objects.filter(gallery__user__id=holder.id, gallery=id)
+        images = Image.objects.filter(gallery__user__id=holder.id, gallery=id)
 
-    return render( request, template, {
+    return render(request, template, {
         'path': request.path,
         'main': main,
         'galleries': galleries,
@@ -71,7 +73,7 @@ def ShowGallery( request, id, user=None ):
         'images': images,
         'user_url': user_url,
         'back': True,
-        } )
+    })
 
 
 @cache_page(60 * 60 * 24)
@@ -80,21 +82,24 @@ def ShowImage( request, id, user=None ):
     """
     Show image with description
     """
-    holder, user_url = get_gallery_user( request, user )
+    holder, user_url = get_gallery_user(request, user)
     if not holder:
-        return ShowMessage( request, message="No user defined" )
+        return ShowMessage(request, message="No user defined")
 
-    image = Image.objects.safe_get( gallery__user__id=holder.id, id=id )
+    if holder == request.user:
+        image = Image.objects.safe_get(gallery__user__id=holder.id, id=id)
+    else:
+        image = Image.objects.safe_get(gallery__user__id=holder.id, id=id, gallery__private=False)
     if image is None:
-        return ShowMessage( request, message="No such image" )
+        return ShowMessage(request, message="No such image")
 
-    return render( request, "core/image.html", {
+    return render(request, "core/image.html", {
         'path': request.path,
         'gallery': image.gallery,
         'image': image,
         'user_url': user_url,
         'back': True,
-        } )
+    })
 
 
 @cache_page(60 * 60 * 24)
@@ -103,21 +108,24 @@ def ShowVideo( request, id, user=None ):
     """
     Show video with description
     """
-    holder, user_url = get_gallery_user( request, user )
+    holder, user_url = get_gallery_user(request, user)
     if not holder:
-        return ShowMessage( request, message="No user defined" )
+        return ShowMessage(request, message="No user defined")
 
-    video = Video.objects.safe_get( gallery__user__id=holder.id, id=id )
+    if holder == request.user:
+        video = Video.objects.safe_get(gallery__user__id=holder.id, id=id)
+    else:
+        video = Video.objects.safe_get(gallery__user__id=holder.id, id=id, gallery__private=False)
     if video is None:
-        return ShowMessage( request, message="No such video" )
+        return ShowMessage(request, message="No such video")
 
-    return render( request, "core/video.html", {
+    return render(request, "core/video.html", {
         'path': request.path,
         'gallery': video.gallery,
         'video': video,
         'user_url': user_url,
         'back': True,
-        } )
+    })
 
 
 @decor_on(settings.VIEWER_SERVE["CACHE"], cache_page, 60 * 60)
@@ -126,27 +134,27 @@ def DownloadVideoThumbnail( request, id, user=None ):
     """
     Get video thumbnail from video hosting and cache it
     """
-    holder, user_url = get_gallery_user( request, user )
+    holder, user_url = get_gallery_user(request, user)
     if not holder:
-        raise Http404( "No user defined" )
+        raise Http404("No user defined")
 
-    video = Video.objects.safe_get( gallery__user__id=holder.id, id=id )
+    video = Video.objects.safe_get(gallery__user__id=holder.id, id=id)
     if video is None:
-        raise Http404( "No such video" )
+        raise Http404("No such video")
     name = video.uid + ".jpg"
     try:
-        options = ResizeOptions( "small", user=holder.url, storage=holder.home, name=str( video.id ) )
-        image_async = CacheImageAsync( video.thumbnail_url, options )
-        image_async.download( )
+        options = ResizeOptions("small", user=holder.url, storage=holder.home, name=str(video.id))
+        image_async = CacheImageAsync(video.thumbnail_url, options)
+        image_async.download()
 
-        response = DownloadResponse.build( image_async.url, name )
+        response = DownloadResponse.build(image_async.url, name)
 
     except ResizeOptionsError as e:
-        logger.error( "id:%s, holder:%s \n %s", id, holder, e )
-        return ShowMessage( request, message=e )
+        logger.error("id:%s, holder:%s \n %s", id, holder, e)
+        return ShowMessage(request, message=e)
     except IOError as e:
-        logger.error( "id:%s, holder:%s \n %s", id, holder, e )
-        raise Http404( "Oops no video thumbnail found" )
+        logger.error("id:%s, holder:%s \n %s", id, holder, e)
+        raise Http404("Oops no video thumbnail found")
 
     return response
 
@@ -157,32 +165,31 @@ def DownloadImage( request, size, id, user=None ):
     """
     Get image with special size
     """
-    holder, user_url = get_gallery_user( request, user )
+    holder, user_url = get_gallery_user(request, user)
     if not holder:
-        raise Http404( "No user defined" )
-    key = "core.views.downloadimage({0},{1})".format(holder.id, id)
-    image = cache.get(key)
-    if image is None:
-        print key
+        raise Http404("No user defined")
+
+    if holder == request.user:
         image = Image.objects.safe_get(gallery__user__id=holder.id, id=id)
-        if image is None:
-            raise Http404( "No such image" )
-        cache.set(key, image, 60 * 60)
+    else:
+        image = Image.objects.safe_get(gallery__user__id=holder.id, id=id, gallery__private=False)
+    if image is None:
+        raise Http404("No such image")
 
-    name = Storage.name( image.path )
+    name = Storage.name(image.path)
     try:
-        options = ResizeOptions( size, user=holder.url, storage=holder.home )
-        image_async = CacheImageAsync( image.path, options )
-        image_async.process( )
+        options = ResizeOptions(size, user=holder.url, storage=holder.home)
+        image_async = CacheImageAsync(image.path, options)
+        image_async.process()
 
-        response = DownloadResponse.build( image_async.url, name )
+        response = DownloadResponse.build(image_async.url, name)
 
     except ResizeOptionsError as e:
-        logger.error( "id:%s, holder:%s \n %s", id, holder, e )
-        return ShowMessage( request, message=e )
+        logger.error("id:%s, holder:%s \n %s", id, holder, e)
+        return ShowMessage(request, message=e)
     except IOError as e:
-        logger.error( "id:%s, holder:%s \n %s", id, holder, e )
-        return redirect( "/static/core/img/gallery.png" )
+        logger.error("id:%s, holder:%s \n %s", id, holder, e)
+        return redirect("/static/core/img/gallery.png")
 
     return response
 
@@ -191,27 +198,27 @@ def ShowMessage( request, title="Error", info=None, message=None ):
     """
     Show simple message with default title="Error", info=None, message=None
     """
-    logger.warning( "title:%s, info:%s \n %s", title, info, message )
-    return render( request, "core/message.html", {
+    logger.warning("title:%s, info:%s \n %s", title, info, message)
+    return render(request, "core/message.html", {
         'path': request.path,
         'title': title,
         'info': info,
         'message': message,
-        } )
+    })
 
 
 def ShowAbout( request, user=None ):
     """
     Show about page
     """
-    holder, user_url = get_gallery_user( request, user )
+    holder, user_url = get_gallery_user(request, user)
     if not holder:
-        return ShowMessage( request, message="No user defined" )
+        return ShowMessage(request, message="No user defined")
 
-    return render( request, "core/about.html", {
+    return render(request, "core/about.html", {
         'path': request.path,
         'avatar': holder.avatar_id,
         'title': holder.about_title,
         'text': holder.about_text,
         'user_url': user_url,
-        } )
+    })
