77f6e459d8b80f68844d72d660fa85726c2b3c8c
B7W <b7w@isudo.ru>
2013-02-06 21:43:11 +0400

    Update profile videos to no ajax

--- bviewer/profile/forms.py
+++ bviewer/profile/forms.py
@@ -2,10 +2,16 @@
 
 from django.forms import ModelForm
 
-from bviewer.core.models import Gallery
+from bviewer.core.models import Gallery, Video
 
 
 class GalleryForm(ModelForm):
     class Meta:
         model = Gallery
         fields = ('title', 'private', 'description', 'time')
+
+
+class VideoForm(ModelForm):
+    class Meta:
+        model = Video
+        fields = ('uid', 'type', 'title', 'description')
