425678ee84355a969c65bdf034ace82364c1ebcd
B7W <isudoblog@gmail.com>
2013-02-11 17:44:34 +0400

    Add return self to Test data for fluent interface. Add test of Video.thumbnail_url. Move all assert to methods call in utils test file.

--- bviewer/core/tests/models.py
+++ bviewer/core/tests/models.py
@@ -1,24 +1,46 @@
 # -*- coding: utf-8 -*-
 
+from httplib import HTTPConnection
+from urlparse import urlsplit
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from bviewer.core.models import ProxyUser, Gallery
+from bviewer.core.models import ProxyUser, Gallery, Video
+from bviewer.core.tests.data import TestData
 
 
 class ModelTest(TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
     def test_new_gallery_user(self):
         """
         Tests domain match
         """
         ProxyUser.objects.create(username='Test', password='secret')
-        assert User.objects.filter(username='Test').exists()
-        assert ProxyUser.objects.filter(username='Test').exists()
-        assert ProxyUser.objects.get(username='Test').top_gallery
-        assert Gallery.objects.filter(user__username='Test').exists()
+        self.assertTrue(User.objects.filter(username='Test').exists())
+        self.assertTrue(ProxyUser.objects.filter(username='Test').exists())
+        self.assertIsNotNone(ProxyUser.objects.get(username='Test').top_gallery)
+        self.assertTrue(Gallery.objects.filter(user__username='Test').exists())
+
+    def assertHttpOk(self, url):
+        """
+        Open http connection, send HEAD and assert 200 status
+        """
+        host = urlsplit(url)
+        conn = HTTPConnection(host.netloc, timeout=4)
+        conn.request('HEAD', host.path)
+        res = conn.getresponse()
+        self.assertEqual(res.status, 200)
+
+    def test_video_thumbnail(self):
+        """
+        Test Vimio 56433514 video and YouTube 7dGGPlZlPQw thumbnails http status.
+        Videos can be deleted! Check it first. Anyway it is important test.
+        """
+        data = TestData().load_users()
+        gallery = data.user_b7w.top_gallery
+
+        video1 = Video.objects.create(gallery=gallery, uid='56433514', type=Video.VIMIO)
+        self.assertHttpOk(video1.thumbnail_url)
+
+        video2 = Video.objects.create(gallery=gallery, uid='7dGGPlZlPQw', type=Video.YOUTUBE)
+        self.assertHttpOk(video2.thumbnail_url)
