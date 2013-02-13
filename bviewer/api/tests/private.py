6ece14f1f5c7c8362a4ececc4c7e6e018ac8f2e6
B7W <b7w@isudo.ru>
2013-02-13 13:32:10 +0400

    Fix wrong test method. Add one more user to TestData, update tests.

--- bviewer/api/tests/private.py
+++ bviewer/api/tests/private.py
@@ -14,32 +14,51 @@ class PrivateTestCase(TestCase):
         self.data = TestData()
         self.data.load_all()
 
+    def login_user(self, user):
+        self.assertTrue(self.client.login(username=user.username, password=TestData.PASSWORD))
+
     def test_galleries(self):
+        # 2 users home + 4 + 1 b7w private
         response = self.client.read('/api/v1/gallery/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(5, len(response.objects))
+        self.assertEqual(len(response.objects), 6)
 
-        self.client.login(username='B7W', password='pass')
+        self.login_user(self.data.user_b7w)
         response = self.client.read('/api/v1/gallery/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(6, len(response.objects))
+        self.assertEqual(len(response.objects), 7)
+
+        self.login_user(self.data.user_keks)
+        response = self.client.read('/api/v1/gallery/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.objects), 6)
 
     def test_images(self):
         response = self.client.read('/api/v1/image/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(2, len(response.objects))
+        self.assertEqual(len(response.objects), 3)
 
-        self.client.login(username='B7W', password='pass')
+        self.login_user(self.data.user_b7w)
         response = self.client.read('/api/v1/image/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(4, len(response.objects))
+        self.assertEqual(len(response.objects), 5)
 
-    def test_images(self):
+        self.login_user(self.data.user_keks)
+        response = self.client.read('/api/v1/image/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.objects), 3)
+
+    def test_videos(self):
         response = self.client.read('/api/v1/video/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(2, len(response.objects))
+        self.assertEqual(len(response.objects), 3)
 
-        self.client.login(username='B7W', password='pass')
+        self.login_user(self.data.user_b7w)
         response = self.client.read('/api/v1/video/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(4, len(response.objects))
+        self.assertEqual(len(response.objects), 5)
+
+        self.login_user(self.data.user_keks)
+        response = self.client.read('/api/v1/video/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.objects), 3)
