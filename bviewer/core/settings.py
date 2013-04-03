6f65cf9943820267f7f780e2a8276adee1388dd8
B7W <b7w@isudo.ru>
2013-04-03 14:47:43 +0300

    Move hard code list of image sises to flexible dict.

--- bviewer/core/settings.py
+++ bviewer/core/settings.py
@@ -5,9 +5,11 @@ from django.conf import settings
 # Some setting that needed for tests
 TESTS = False
 
+
 # If gallery is used for one person or for tests
 # set user id here
 VIEWER_USER_ID = getattr(settings, 'VIEWER_USER_ID', None)
+
 
 # Path where cache will bw stored
 # It is a resize images so it can be huge
@@ -16,27 +18,36 @@ VIEWER_USER_ID = getattr(settings, 'VIEWER_USER_ID', None)
 # each user have his own sub folder
 VIEWER_CACHE_PATH = getattr(settings, 'VIEWER_CACHE_PATH', '../cache')
 
+
 # Start path where full images are located
 # each user will have his own home, in profile.storage
 VIEWER_STORAGE_PATH = getattr(settings, 'VIEWER_STORAGE_PATH')
 
-VIEWER_SMALL_SIZE = getattr(settings, 'VIEWER_SMALL_SIZE', {
-    'WIDTH': 300,
-    'HEIGHT': 300,
-    'CROP': True,
+
+# Sizes to resize images.
+# Is it a map of maps. example - `'small': {'WIDTH': 300, 'HEIGHT': 300, 'CROP': True}`
+# By default crop is False, on True edges cut off.
+# if image smaller than size it will be linked.
+VIEWER_IMAGE_SIZE = getattr(settings, 'VIEWER_IMAGE_SIZE', {
+    'small': {
+        'WIDTH': 300,
+        'HEIGHT': 300,
+        'CROP': True,
+    },
+    'middle': {
+        'WIDTH': 870,
+        'HEIGHT': 600,
+    },
+    'big': {
+        'WIDTH': 1280,
+        'HEIGHT': 720,
+    },
+    'full': {
+        'WIDTH': 10 ** 6,
+        'HEIGHT': 10 ** 6,
+    },
 })
 
-VIEWER_MIDDLE_SIZE = getattr(settings, 'VIEWER_MIDDLE_SIZE', {
-    'WIDTH': 870,
-    'HEIGHT': 600,
-    'CROP': False,
-})
-
-VIEWER_BIG_SIZE = getattr(settings, 'VIEWER_BIG_SIZE', {
-    'WIDTH': 1280,
-    'HEIGHT': 720,
-    'CROP': False,
-})
 
 # X-Accel-Redirect for web server to improve file serving, highly recommended!
 # Set cache true to activate redirect response caching, it save 2 queries per image.
