b7558c678a2b9db1faa4e439c46cb722befd3402
B7W <isudoblog@gmail.com>
2013-04-14 19:31:51 +0400

    Fix cache decorator error. Fix simple json an unicode strings in archive app. Mix a little url.

--- bviewer/archive/urls.py
+++ bviewer/archive/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^gallery/(?P<id>[\w]+)/archive/$', 'bviewer.archive.views.Archive', name='archive.archive'),
-    url(r'^gallery/(?P<id>[\w]+)/archive/(?P<hash>[\w]+)/download/$', 'bviewer.archive.views.Download', name='archive.download'),
-    url(r'^gallery/(?P<id>[\w]+)/archive/(?P<hash>[\w]+)/status/$', 'bviewer.archive.views.ArchiveStatus', name='archive.status'),
+    url(r'^archive/(?P<id>[\w]+)/$', 'bviewer.archive.views.Archive', name='archive.archive'),
+    url(r'^archive/(?P<id>[\w]+)/(?P<hash>[\w]+)/download/$', 'bviewer.archive.views.Download', name='archive.download'),
+    url(r'^archive/(?P<id>[\w]+)/(?P<hash>[\w]+)/status/$', 'bviewer.archive.views.ArchiveStatus', name='archive.status'),
 )
