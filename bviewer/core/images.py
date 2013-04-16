17b4de3dea182232c34f9e7576f1fa43e75c9eec
B7W <b7w@isudo.ru>
2013-04-16 11:04:10 +0400

    Add quality option to image settings. Refactor ResizeImage more pythonic.

--- bviewer/core/images.py
+++ bviewer/core/images.py
@@ -25,7 +25,6 @@ class ResizeImage(object):
         self.file = Image.open(filein)
         if self.file.mode not in ('L', 'RGB'):
             self.file = self.file.convert('RGB')
-        self.quality = 95
         self.type = 'JPEG'
 
     @property
@@ -44,47 +43,47 @@ class ResizeImage(object):
 
     def resize(self, width, height):
         """
-        Resize image to ``width`` and ``width``
+        Resize image to `width` and `width`
         """
         self.file = self.file.resize((width, height), Image.ANTIALIAS)
 
     def crop(self, x_offset, y_offset, width, height):
         """
-        Crop image with ``x_offset``, ``y_offset``, ``width``, ``height``
+        Crop image with `x_offset`, `y_offset`, `width`, `height`
         """
         self.file = self.file.crop((x_offset, y_offset, width, height))
 
-    def cropCenter(self, width, height):
+    def crop_center(self, width, height):
         """
-        Cut out an image with ``width`` and ``height`` of the center
+        Cut out an image with `width` and `height` of the center
         """
         x_offset = (self.width - width) / 2
         y_offset = (self.height - height) / 2
         self.crop(x_offset, y_offset, x_offset + width, y_offset + height)
 
-    def isPortrait(self):
+    def is_portrait(self):
         """
         Is width < height
         """
         return self.width < self.height
 
-    def isLandscape(self):
+    def is_landscape(self):
         """
         Is width >= height
         """
         return self.width >= self.height
 
-    def isBigger(self, width, height):
+    def is_bigger(self, width, height):
         """
-        Is this image bigger that ``width`` and ``height``
+        Is this image bigger that `width` and `height`
         """
         return self.width > width and self.height > height
 
-    def minSize(self, value):
+    def min_size(self, value):
         """
-        Scale images size where the min size len will be ``value``
+        Scale images size where the min size len will be `value`
         """
-        if self.isLandscape():
+        if self.is_landscape():
             scale = float(self.height) / value
             width = int(self.width / scale)
             return width, value
@@ -93,11 +92,11 @@ class ResizeImage(object):
             height = int(self.height / scale)
             return value, height
 
-    def maxSize(self, value):
+    def max_size(self, value):
         """
-        Scale images size where the max size len will be ``value``
+        Scale images size where the max size len will be `value`
         """
-        if self.isPortrait():
+        if self.is_portrait():
             scale = float(self.height) / value
             width = int(self.width / scale)
             return width, value
@@ -106,11 +105,14 @@ class ResizeImage(object):
             height = int(self.height / scale)
             return value, height
 
-    def saveTo(self, fileio):
+    def save_to(self, fout, quality):
         """
-        Save to open file ``fileio``. Need to close by yourself.
+        Save to open file. Need to close by yourself.
+
+        :type fout: file
+        :type quality: int
         """
-        self.file.save(fileio, self.type, quality=self.quality)
+        self.file.save(fout, self.type, quality=quality)
 
 
 class CacheImage(object):
@@ -120,8 +122,8 @@ class CacheImage(object):
 
     def __init__(self, path, options):
         """
-        path -> path to image,
-        options -> ResizeOptions
+        :type path: str
+        :type options: bviewer.core.utils.ResizeOptions
         """
         self.path = path
         self.abs_path = os.path.join(settings.VIEWER_STORAGE_PATH, options.storage, path)
@@ -135,15 +137,16 @@ class CacheImage(object):
 
     def get_hash_name(self):
         self.hash_builder = FileUniqueName()
+        options = (self.options.size, self.options.quality)
         # if it is a video - there is no file path
         if self.options.name:
-            return self.hash_builder.build(self.options.name, extra=self.options.size)
+            return self.hash_builder.build(self.options.name, extra=options)
         try:
             time = os.path.getmtime(self.abs_path)
         except OSError as e:
             # TODO: fix this terrible code
             raise IOError(e)
-        return self.hash_builder.build(self.path, time=time, extra=self.options.size)
+        return self.hash_builder.build(self.path, time=time, extra=options)
 
     def process(self):
         """
@@ -154,17 +157,17 @@ class CacheImage(object):
         if not os.path.lexists(self.cache):
             with open(self.abs_path, mode='rb') as filein:
                 new_image = ResizeImage(filein)
-                bigger = new_image.isBigger(self.options.width, self.options.height)
+                bigger = new_image.is_bigger(self.options.width, self.options.height)
                 if bigger:
                     if self.options.crop:
-                        w, h = new_image.minSize(self.options.size)
+                        w, h = new_image.min_size(self.options.size)
                         new_image.resize(w, h)
-                        new_image.cropCenter(self.options.width, self.options.height)
+                        new_image.crop_center(self.options.width, self.options.height)
                     else:
-                        w, h = new_image.maxSize(self.options.size)
+                        w, h = new_image.max_size(self.options.size)
                         new_image.resize(w, h)
-                    with open(self.cache, mode='wb') as fileout:
-                        new_image.saveTo(fileout)
+                    with open(self.cache, mode='wb') as fout:
+                        new_image.save_to(fout, self.options.quality)
                     logger.info('resize \'%s\' with %s', self.path, self.options)
                 else:
                     logger.info('link \'%s\' with %s', self.path, self.options)
@@ -181,13 +184,13 @@ class CacheImage(object):
             image.write(urllib2.urlopen(self.path).read())
             image.seek(0)
             newImage = ResizeImage(image)
-            bigger = newImage.isBigger(self.options.width, self.options.height)
+            bigger = newImage.is_bigger(self.options.width, self.options.height)
             if bigger:
-                w, h = newImage.minSize(self.options.size)
+                w, h = newImage.min_size(self.options.size)
                 newImage.resize(w, h)
-                newImage.cropCenter(self.options.width, self.options.height)
+                newImage.crop_center(self.options.width, self.options.height)
                 with open(self.cache, mode='wb') as fileout:
-                    newImage.saveTo(fileout)
+                    newImage.save_to(fileout, self.options.quality)
             logger.info('download image \'%s\' %s', self.path, 'and resize' if bigger else '')
 
     def check_cache_dir(self):
