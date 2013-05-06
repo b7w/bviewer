985bd614fb182423fdff27e4c4f49178b391ba28
B7W <b7w@isudo.ru>
2013-05-06 18:17:21 +0400

    Fix inheritance error in FileImageReader. Add process and download CacheImage tests.

--- bviewer/core/tests/images.py
+++ bviewer/core/tests/images.py
@@ -2,7 +2,7 @@
 from django.test import TestCase
 
 from bviewer.core import settings
-from bviewer.core.images import CacheImage
+from bviewer.core.images import CacheImage, FileImageReader
 from bviewer.core.utils import ResizeOptions
 from bviewer.core.tests.data import TestData
 
@@ -34,3 +34,47 @@ class CacheImageTest(TestCase):
         self.data._generate_image(user.home, image.path, force=True)
         hash2 = CacheImage(image.path, option).hash
         self.assertNotEqual(hash1, hash2, msg='Check hash eq after new image creation')
+
+    def process_image(self, test_func=None):
+        image = self.data.image1
+        user = image.gallery.user
+
+        options_list = [
+            ResizeOptions(user, 512, 512, crop=False, quality=95),
+            ResizeOptions(user, 512, 2048, crop=True, quality=95),
+            ResizeOptions(user, 1024, 2048, crop=False, quality=95),
+            ResizeOptions(user, 4096, 4096, crop=False, quality=95),
+        ]
+
+        for options in options_list:
+            test_func(image, options)
+
+    def test_process(self):
+        def local(image, options):
+            cache_image = CacheImage(image.path, options)
+            cache_image.clear_cache()
+            self.assertFalse(cache_image.is_exists())
+
+            cache_image.process()
+            self.assertTrue(cache_image.is_exists())
+
+            cache_image.clear_cache()
+            self.assertFalse(cache_image.is_exists())
+
+        self.process_image(local)
+
+    def test_process(self):
+        def local(image, options):
+            cache_image = CacheImage(image.path, options)
+            #hack to use FileImageReader
+            cache_image.path = cache_image.abs_path
+            cache_image.clear_cache()
+            self.assertFalse(cache_image.is_exists())
+
+            cache_image.process(reader=FileImageReader)
+            self.assertTrue(cache_image.is_exists())
+
+            cache_image.clear_cache()
+            self.assertFalse(cache_image.is_exists())
+
+        self.process_image(local)
