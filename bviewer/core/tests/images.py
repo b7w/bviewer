be2151e9ce4ed25bdf5748dd78e7536b3faca303
B7W <b7w@isudo.ru>
2013-05-07 10:19:31 +0400

    Mock all dependencies in CacheImageTest. Rename TestData.generate_image to public.

--- bviewer/core/tests/images.py
+++ bviewer/core/tests/images.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from mock import Mock
 
-from bviewer.core import settings
 from bviewer.core.images import CacheImage, FileImageReader
 from bviewer.core.utils import ResizeOptions
 from bviewer.core.tests.data import TestData
@@ -10,48 +10,43 @@ from bviewer.core.tests.data import TestData
 class CacheImageTest(TestCase):
     def setUp(self):
         self.data = TestData()
-        self.data.load_all()
-        settings.VIEWER_USER_ID = self.data.user_b7w.id
+        self.path = 'image1.jpg'
+        self.user = Mock(url='b7w.dev.loc', home='')
+        self.data.generate_image(self.user.home, self.path)
 
     def test_hash(self):
-        image = self.data.image1
-        user = image.gallery.user
-
         options = [
-            ResizeOptions(user, 32, 32, crop=False, quality=95),
-            ResizeOptions(user, 64, 32, crop=False, quality=95),
-            ResizeOptions(user, 32, 64, crop=False, quality=95),
-            ResizeOptions(user, 32, 32, crop=True, quality=95),
-            ResizeOptions(user, 32, 32, crop=False, quality=100),
+            ResizeOptions(self.user, 32, 32, crop=False, quality=95),
+            ResizeOptions(self.user, 64, 32, crop=False, quality=95),
+            ResizeOptions(self.user, 32, 64, crop=False, quality=95),
+            ResizeOptions(self.user, 32, 32, crop=True, quality=95),
+            ResizeOptions(self.user, 32, 32, crop=False, quality=100),
         ]
 
-        hashes = set(CacheImage(image.path, i).hash for i in options)
+        hashes = set(CacheImage(self.path, i).hash for i in options)
         self.assertEqual(len(options), len(hashes),
             msg='Check unique hashes for different options')
 
-        option = ResizeOptions(user, 128, 128, crop=False, quality=95)
-        hash1 = CacheImage(image.path, option).hash
-        self.data._generate_image(user.home, image.path, force=True)
-        hash2 = CacheImage(image.path, option).hash
+        option = ResizeOptions(self.user, 128, 128, crop=False, quality=95)
+        hash1 = CacheImage(self.path, option).hash
+        self.data.generate_image(self.user.home, self.path, force=True)
+        hash2 = CacheImage(self.path, option).hash
         self.assertNotEqual(hash1, hash2, msg='Check hash eq after new image creation')
 
     def process_image(self, test_func=None):
-        image = self.data.image1
-        user = image.gallery.user
-
         options_list = [
-            ResizeOptions(user, 512, 512, crop=False, quality=95),
-            ResizeOptions(user, 512, 2048, crop=True, quality=95),
-            ResizeOptions(user, 1024, 2048, crop=False, quality=95),
-            ResizeOptions(user, 4096, 4096, crop=False, quality=95),
+            ResizeOptions(self.user, 512, 512, crop=False, quality=95),
+            ResizeOptions(self.user, 512, 2048, crop=True, quality=95),
+            ResizeOptions(self.user, 1024, 2048, crop=False, quality=95),
+            ResizeOptions(self.user, 4096, 4096, crop=False, quality=95),
         ]
 
         for options in options_list:
-            test_func(image, options)
+            test_func(self.path, options)
 
     def test_process(self):
-        def local(image, options):
-            cache_image = CacheImage(image.path, options)
+        def local(path, options):
+            cache_image = CacheImage(path, options)
             cache_image.clear_cache()
             self.assertFalse(cache_image.is_exists())
 
@@ -64,8 +59,8 @@ class CacheImageTest(TestCase):
         self.process_image(local)
 
     def test_process(self):
-        def local(image, options):
-            cache_image = CacheImage(image.path, options)
+        def local(path, options):
+            cache_image = CacheImage(path, options)
             #hack to use FileImageReader
             cache_image.path = cache_image.abs_path
             cache_image.clear_cache()
