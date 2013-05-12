cecf62123d1be4e8fa65fc07d5e03864a9e4ee31
B7W <isudoblog@gmail.com>
2013-05-12 19:04:56 +0400

    Rename ResourceClient read to rest_get

--- bviewer/api/tests/advance.py
+++ bviewer/api/tests/advance.py
@@ -6,14 +6,14 @@ from bviewer.api.tests.base import BaseResourceTestCase
 class AdvanceTestCase(BaseResourceTestCase):
     def test_filter_self(self):
         self.login_user(self.data.user_b7w)
-        response = self.client.read('/api/v1/gallery/?user=self')
+        response = self.client.rest_get('/api/v1/gallery/?user=self')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 5)
 
-        response = self.client.read('/api/v1/image/?gallery__user=self')
+        response = self.client.rest_get('/api/v1/image/?gallery__user=self')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 4)
 
-        response = self.client.read('/api/v1/image/?gallery__user=self')
+        response = self.client.rest_get('/api/v1/image/?gallery__user=self')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 4)
--- bviewer/api/tests/private.py
+++ bviewer/api/tests/private.py
@@ -6,7 +6,7 @@ from bviewer.api.tests.base import BaseResourceTestCase
 class PrivateTestCase(BaseResourceTestCase):
     def test_galleries(self):
         # 2 users home + 4 + 1 b7w private
-        response = self.client.read('/api/v1/gallery/')
+        response = self.client.rest_get('/api/v1/gallery/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 6)
 
@@ -15,18 +15,18 @@ class PrivateTestCase(BaseResourceTestCase):
         self.assertEqual(response.status_code, 401)
 
         self.login_user(self.data.user_b7w)
-        response = self.client.read('/api/v1/gallery/')
+        response = self.client.rest_get('/api/v1/gallery/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 7)
 
         self.login_user(self.data.user_keks)
-        response = self.client.read('/api/v1/gallery/')
+        response = self.client.rest_get('/api/v1/gallery/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 6)
 
     def test_images(self):
         # gallery 1 and 5, other private
-        response = self.client.read('/api/v1/image/')
+        response = self.client.rest_get('/api/v1/image/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 3)
 
@@ -36,19 +36,19 @@ class PrivateTestCase(BaseResourceTestCase):
 
         # plus 2 private
         self.login_user(self.data.user_b7w)
-        response = self.client.read('/api/v1/image/')
+        response = self.client.rest_get('/api/v1/image/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 5)
 
         # gallery 1 and 5, other private
         self.login_user(self.data.user_keks)
-        response = self.client.read('/api/v1/image/')
+        response = self.client.rest_get('/api/v1/image/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 3)
 
     def test_videos(self):
         # same as images
-        response = self.client.read('/api/v1/video/')
+        response = self.client.rest_get('/api/v1/video/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 3)
 
@@ -57,27 +57,27 @@ class PrivateTestCase(BaseResourceTestCase):
         self.assertEqual(response.status_code, 401)
 
         self.login_user(self.data.user_b7w)
-        response = self.client.read('/api/v1/video/')
+        response = self.client.rest_get('/api/v1/video/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 5)
 
         self.login_user(self.data.user_keks)
-        response = self.client.read('/api/v1/video/')
+        response = self.client.rest_get('/api/v1/video/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.objects), 3)
 
     def test_image_field_path(self):
         url = '/api/v1/image/{0}/'.format(self.data.image1.id)
-        response = self.client.read(url)
+        response = self.client.rest_get(url)
         self.assertEqual(response.status_code, 200)
         self.assertNotIn('path', response.object)
 
         self.login_user(self.data.user_b7w)
-        response = self.client.read(url)
+        response = self.client.rest_get(url)
         self.assertEqual(response.status_code, 200)
         self.assertIn('path', response.object)
 
         self.login_user(self.data.user_keks)
-        response = self.client.read(url)
+        response = self.client.rest_get(url)
         self.assertEqual(response.status_code, 200)
         self.assertNotIn('path', response.object)
