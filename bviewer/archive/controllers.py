4547b2f2b3f0c568ca4e5c143f93a453b7b61efe
B7W <b7w@isudo.ru>
2013-07-04 15:37:25 +0400

    Fix archive creation, now progress stored int redis

--- bviewer/archive/controllers.py
+++ bviewer/archive/controllers.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 
+import django_rq
 from django.conf import settings as django_settings
-from django_rq import get_queue
 
 from bviewer.core.files.storage import ImageStorage
 from bviewer.core.utils import ResizeOptions, cache_method
@@ -12,6 +12,8 @@ logger = logging.getLogger(__name__)
 
 
 class ZipArchiveController(object):
+    STATUS_KEY_TIMOUT = 4 * 60
+
     def __init__(self, image_paths, holder, name=None):
         """
         :type image_paths: list of str
@@ -37,7 +39,7 @@ class ZipArchiveController(object):
         """
         if self.archive.cache_exists:
             return 'DONE'
-        elif self.archive.cache_exists:
+        if 0 <= self.progress <= 100:
             return 'PROCESSING'
         return 'NONE'
 
@@ -50,32 +52,37 @@ class ZipArchiveController(object):
         return self.storage.hash_for(tuple(pack))
 
     @property
+    def _redis_uid(self):
+        return 'bviewer.archive.status:{0}'.format(self.uid)
+
+    @property
     def progress(self):
         """
-        Progress in range of 0..100
+        Get progress from redis. Range 0..100, if None return -1.
 
         :rtype: int
         """
-        if self.status == 'DONE':
-            return 100
-        if self.status == 'PROCESSING':
-            done = self.archive.cache_ctime
-
-            f = lambda path: self.storage.get_path(path).ctime
-            real = sum(f(i.path) for i in self.image_paths)
-            return int(float(done) / real * 100)
-        if self.status == 'NONE':
-            return 0
+        redis = django_rq.get_connection()
+        value = redis.get(self._redis_uid)
+        return int(value) if value is not None else -1
 
     def process(self):
         """
         Process if `self.status` == NONE
+        Update redis progress each file.
         """
+        lenght = len(self.image_paths)
+        # local otherwise not pickle
+        redis = django_rq.get_connection()
+
         if self.status == 'NONE':
             with self.archive.cache_open() as z:
-                for image_path in self.image_paths:
+                for i, image_path in enumerate(self.image_paths, start=1):
                     with image_path.open(mode='rb') as f:
                         z.writestr(image_path.name, f.read())
+
+                    percent = int(float(i) / lenght * 100)
+                    redis.setex(self._redis_uid, percent, time=self.STATUS_KEY_TIMOUT)
 
             self.archive.rename_temp_cache()
 
@@ -86,5 +93,5 @@ class ZipArchiveController(object):
         if django_settings.TEST:
             self.process()
         else:
-            rq = get_queue(name='low')
+            rq = django_rq.get_queue(name='low')
             rq.enqueue(self.process)
