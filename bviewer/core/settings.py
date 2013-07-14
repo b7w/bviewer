e36f9efa00153e3d786a29bb9a3eed78c0a331c5
B7W <isudoblog@gmail.com>
2013-07-15 00:47:56 +0400

    Fix some bugs in settings. Change order in management docs.

--- bviewer/core/settings.py
+++ bviewer/core/settings.py
@@ -49,7 +49,7 @@ VIEWER_IMAGE_SIZE = getattr(settings, 'VIEWER_IMAGE_SIZE', {
 
 # X-Accel-Redirect for web server to improve file serving, highly recommended!
 # Set cache true to activate redirect response caching, it save 2 queries per image.
-# Be careful, cache can't work with `default`! because it return hole file
+# Be careful, cache can't work with `django`! because it return hole file
 VIEWER_DOWNLOAD_RESPONSE = getattr(settings, 'VIEWER_DOWNLOAD_RESPONSE', {
     'BACKEND': 'bviewer.core.files.response.django',
     'INTERNAL_URL': '/protected',
