f8903cab61994af5c80ee5f6ad7b492f67173a8f
B7W <isudoblog@gmail.com>
2013-09-03 15:12:40 +0400

    Refactor url

--- bviewer/archive/urls.py
+++ bviewer/archive/urls.py
@@ -2,8 +2,8 @@
 
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^archive/(?P<gid>[\w]+)/$', 'bviewer.archive.views.index_view', name='archive.archive'),
-    url(r'^archive/(?P<gid>[\w]+)/(?P<uid>[\w]+)/download/$', 'bviewer.archive.views.download_view', name='archive.download'),
-    url(r'^archive/(?P<gid>[\w]+)/(?P<uid>[\w]+)/status/$', 'bviewer.archive.views.status_view', name='archive.status'),
+urlpatterns = patterns('bviewer.archive.views',
+    url(r'^(?P<gid>[\w]+)/$', 'index_view', name='archive.archive'),
+    url(r'^(?P<gid>[\w]+)/(?P<uid>[\w]+)/download/$', 'download_view', name='archive.download'),
+    url(r'^(?P<gid>[\w]+)/(?P<uid>[\w]+)/status/$', 'status_view', name='archive.status'),
 )
--- bviewer/urls.py
+++ bviewer/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('bviewer.core.urls')),
-    url(r'^', include('bviewer.archive.urls')),
+    url(r'^archive/', include('bviewer.archive.urls')),
     url(r'^api/', include('bviewer.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^profile/', include('bviewer.profile.urls')),
