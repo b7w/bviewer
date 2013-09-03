f7432af1978df8591ac9321f0f8fd7907dde4346
B7W <b7w@isudo.ru>
2013-09-03 15:12:40 +0400

    Refactor url

--- bviewer/core/urls.py
+++ bviewer/core/urls.py
@@ -2,14 +2,14 @@
 
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'bviewer.core.views.index_view', name='core.home'),
-    url(r'^about/$', 'bviewer.core.views.about_view', name='core.about'),
-    url(r'^login/$', 'bviewer.core.views.login_view', name='core.login'),
-    url(r'^logout/$', 'bviewer.core.views.logout_view', name='core.logout'),
-    url(r'^gallery/(?P<uid>[\w]+)/$', 'bviewer.core.views.gallery_view', name='core.gallery'),
-    url(r'^image/(?P<uid>\w+)/$', 'bviewer.core.views.image_view', name='core.image'),
-    url(r'^video/(?P<uid>\w+)/$', 'bviewer.core.views.video_view', name='core.video'),
-    url(r'^video/(?P<uid>\w+)/thumbnail/$', 'bviewer.core.views.download_video_thumbnail_view', name='core.video.thumbnail'),
-    url(r'^download/(?P<size>\w+)/(?P<uid>\w+).jpg$', 'bviewer.core.views.download_image_view', name='core.download'),
+urlpatterns = patterns('bviewer.core.views',
+    url(r'^$', 'index_view', name='core.home'),
+    url(r'^about/$', 'about_view', name='core.about'),
+    url(r'^login/$', 'login_view', name='core.login'),
+    url(r'^logout/$', 'logout_view', name='core.logout'),
+    url(r'^gallery/(?P<uid>[\w]+)/$', 'gallery_view', name='core.gallery'),
+    url(r'^image/(?P<uid>\w+)/$', 'image_view', name='core.image'),
+    url(r'^video/(?P<uid>\w+)/$', 'video_view', name='core.video'),
+    url(r'^video/(?P<uid>\w+)/thumbnail/$', 'download_video_thumbnail_view', name='core.video.thumbnail'),
+    url(r'^download/(?P<size>\w+)/(?P<uid>\w+).jpg$', 'download_image_view', name='core.download'),
 )
