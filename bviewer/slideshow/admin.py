4f48f0879b1935c5dfab84d51ff776e713f04647
B7W <b7w@isudo.ru>
2013-11-30 21:49:46 +0400

    Add profile and admin views for slideshow

---
+++ bviewer/slideshow/admin.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from django.contrib.admin import site
+
+from bviewer.profile.admin import profile
+from bviewer.core.models import Gallery
+from bviewer.profile.admin import ProfileModelAdmin
+from bviewer.slideshow.models import SlideShow
+
+
+class SlideShowAdmin(ProfileModelAdmin):
+    list_select_related = True
+
+    list_display = ('id', 'gallery_title', 'session_key', 'status', 'image_count', 'time', )
+    list_filter = ('gallery__title', 'time',)
+    ordering = ('-time', )
+
+    search_fields = ('gallery__title', 'status',)
+
+    def gallery_title(self, obj):
+        return obj.gallery.title
+
+
+site.register(SlideShow, SlideShowAdmin)
+
+
+class SlideShowProfile(SlideShowAdmin):
+    list_display = ('gallery_title', 'session_key', 'status', 'image_count', 'time', )
+
+    def queryset(self, request):
+        return super(SlideShowProfile, self).queryset(request).filter(gallery__user=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        """
+        Show in drop down menu only user galleries
+        """
+        if db_field.name == 'gallery':
+            kwargs['queryset'] = Gallery.objects.filter(user=request.user)
+        return super(SlideShowProfile, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(SlideShowProfile, self).get_form(request, obj, **kwargs)
+        form.base_fields['session_key'].initial = request.session.session_key
+        return form
+
+
+profile.register(SlideShow, SlideShowProfile)
+
+
