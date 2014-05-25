af64d551ced43549cfabad01f71095524f6a1206
B7W <bviewer@isudo.ru>
2014-05-25 23:59:39 +0400

    Pre cache function

--- bviewer/profile/urls.py
+++ bviewer/profile/urls.py
@@ -4,8 +4,9 @@ from django.conf.urls import patterns, url, include
 from bviewer.profile.admin import profile
 
 
-urlpatterns = patterns('',
-    url(r'^gallery/(?P<uid>\w+)/$', 'bviewer.profile.views.images_view', name='profile.gallery'),
-    url(r'^download/$', 'bviewer.profile.views.download_image', name='profile.download'),
+urlpatterns = patterns('bviewer.profile.views',
+    url(r'^gallery/(?P<uid>\w+)/$', 'images_view', name='profile.gallery'),
+    url(r'^gallery/(?P<uid>\w+)/pre-cache/$', 'gallery_pre_cache', name='profile.gallery.pre-cache'),
+    url(r'^download/$', 'download_image', name='profile.download'),
     url(r'^', include(profile.urls)),
 )
